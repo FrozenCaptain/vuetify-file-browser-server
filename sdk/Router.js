c5bb04855a48a0068b54d9e54fae3e8bb1c5b006
Maurits Schuurman <FrozenCaptain@users.noreply.github.com>
2021-10-07 22:33:53 +0200

    nice

--- sdk/Router.js
+++ sdk/Router.js
@@ -25,7 +25,8 @@ module.exports = function (storages, options = {}) {
         // `view` endpoint
         router.get(`/${storage.code}/view`, async function (req, res) {
             let filename = await storage.view(req.query.path);
-            return  res.sendFile(filename);
+            //return  res.sendFile(filename);
+            return  filename;
         });
 
         // `delete` endpoint
